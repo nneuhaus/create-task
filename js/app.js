73769b9e545af42159a082fb47a029dbc7eb662e
unknown <nneuhaus@ucvts.org>
2020-04-02 13:12:32 -0400

    forgot src

---
+++ js/app.js
@@ -0,0 +1,6 @@
+///////
+
+const topleft= document.getElementByID("#topleft");
+const topright= document.getElementByID("#topright");
+const bottomleft= document.getElementByID("#bottomleft");
+const bottomright=document.getElementByID("#bottomright");
