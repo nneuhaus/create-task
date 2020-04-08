854529b573fc8c6095937434699a61d1d8d636dc
unknown <nneuhaus@ucvts.org>
2020-04-08 12:41:03 -0400

    added start button, commit 11

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
-///////
-
+///////////////////// CONSTANTS /////////////////////////////////////
 const topleft= document.querySelector("#topleft");
 const topright= document.querySelector("#topright");
 const bottomleft= document.querySelector("#bottomleft");
 const bottomright=document.querySelector("#bottomright");
+const start=document.querySelector("#start")
+///////////////////// APP STATE (VARIABLES) /////////////////////////
+let order = [] //array
+let turn;
+let on = false;
+///////////////////// CACHED ELEMENT REFERENCES /////////////////////
+
+///////////////////// EVENT LISTENERS ///////////////////////////////
